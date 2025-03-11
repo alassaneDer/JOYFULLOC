7ab53f568026f9b36a0fc959655655cf3b2f3f33
alassane der <alassaneibrahimder@gmail.com>
2025-03-11 23:40:45 +0100

    Initial Commit

---
+++ JoyFullOC/JoyFullOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyFullOCApp.swift
+//  JoyFullOC
+//
+//  Created by Alassane Der on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct JoyFullOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
